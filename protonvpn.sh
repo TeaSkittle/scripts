ef26f17eea58eac933a85d75810eff2d03c9e94c
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-06-01 14:23:05 -0600

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/sh
+# (c) Travis Dowd
+# 
 # Install and configure proton VPN (debian-based)
+#
 sudo apt install -y openvpn dialog python3-pip python3-setuptools
 sudo pip3 install protonvpn-cli
 echo "Use OpenVPN account settings, not ProtonVPN."
 echo "Log into protonvpn.com to find OpenVPN account and password"
+echo "[+] Opening protonvpn login in browser..."
+xdg-open https://account.protonvpn.com/ || echo "[!] Error: Can't open URL"
+read -r "Press [Enter] to resume ..."
 sudo protonvpn init
-echo "Connecting to fastest VPN server..."
+echo "[+] Connecting to fastest VPN server..."
 sudo protonvpn c -f
 echo "Connection status:"
 sudo protonvpn status
