89be1267d4794acd5f2ea8f401a2a0970d28eb81
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2020-06-01 14:44:18 -0600

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -3,6 +3,12 @@
 # 
 # Install and configure proton VPN (debian-based)
 #
+#
+# IMPORTANT NOTE:
+# According to man xdg-open this should not be run as root, because it launches a GUI browser
+# more info here: https://askubuntu.com/questions/270006/why-should-users-never-use-normal-sudo-to-start-graphical-applications
+# may try and open with lynx, or start script with gksudo instead of sudo
+#
 apt install -y openvpn dialog python3-pip python3-setuptools
 pip3 install protonvpn-cli
 echo "IMPORTANT: Use OpenVPN account settings, not ProtonVPN! Can be found on protonvpn.com"
