46a4f139850b21c7cd4eae96246457d38a4e3b0f
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-02-22 14:50:42 -0700

    Update 100_disk.bat

--- 100_disk.bat
+++ 100_disk.bat
@@ -3,50 +3,24 @@
 :: Date: 12-22-2020   
 ::
 :: Description: Fix Disk running at 100% usage in Windows 10
-::
+::  A hacky solution to permanetly fix:
+::   1. Place script at C:\100_sisk.bat
+::   2. Create shorcut to script and force admin: https://www.cnet.com/how-to/always-run-a-program-in-administrator-mode-in-windows-10/
+::   3. Place shortcut in shell:startup
 :: ==================================================================================
 @ECHO OFF
-CLS
-SETLOCAL EnableDelayedExpansion
-COLOR 0F
-:: Check for admin rights
-NET SESSION >NUL 2>&1
-IF %errorLevel% == 0 (
-    ECHO.
-) ELSE (
-    COLOR 0C
-    ECHO Run as Admin!
-    PAUSE
-    COLOR 0F
-    EXIT /B 0
-)
 :: Disable services
-net stop "Windows search"
-net stop superfetch
-net stop w32time
-w32tm /unregister
-w32tm /register
-net start w32time
-sc config wuauserv  start=auto
-sc config w32time   start=auto
-sc config bits      start=auto
-sc config cryptsvc  start=auto
-sc config trustedinstaller start= auto
+net.exe stop superfetch
 sc config superfetch start=disabled
-sc config "Windows search" start=disabled
-:: Delete temp files
-del /f /s /q %temp%  
-:: Check and fix disk errors
-wmic diskdrive get status
-sfc /scannow
-chkdsk /f /r
-defrag c: /u /v  
-:: Set power plan to "High Performance"
-powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-:: Disable Windows tips
-reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /t REG_DWORD /v SubscribedContent-338389Enabled /d 0 /f
-:: Configure virtual memory
-wmic pagefile list /format:list
-wmic computersystem where name="%computername%" set AutomaticManagedPagefile=true 
-:: Restart machine
-shutdown /r /t 00
+net.exe stop "Windows search"
+sc config "WSearch" start=disabled
+sc stop "WSearch"
+net.exe stop "DiagTrack"
+sc config "DiagTrack" start=disabled
+sc stop "DiagTrack"
+net.exe stop "wuauserv"
+sc config "wuauserv" start=disabled
+sc stop "wuauserv"
+net.exe stop "BITS"
+sc config "BITS" start=disabled
+sc stop "BITS"
